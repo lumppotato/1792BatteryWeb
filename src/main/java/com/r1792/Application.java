35fc7dbcd6874c32f4dd5249f998a12fcbe141da
greg <ocpistol@gmail.com>
2026-02-06 14:36:29 -0600

    A0.0.3 - Prepairing for dev deployment server

--- src/main/java/com/r1792/Application.java
+++ src/main/java/com/r1792/Application.java
@@ -2,21 +2,10 @@ package com.r1792;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(Application.class);
-    }
-
+public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
-
-
 }
